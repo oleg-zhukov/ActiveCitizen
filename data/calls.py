6f1ebdff450a2226dde024e0c745e9dac421989e
Alexander Shukhman <shukhman@gmail.com>
2021-05-07 23:04:41 +0500

    Initial commit

---
+++ data/calls.py
@@ -0,0 +1,80 @@
+import datetime
+import sqlalchemy
+import requests
+from sqlalchemy import orm
+from sqlalchemy_serializer import SerializerMixin
+from .db_session import SqlAlchemyBase
+
+def predict_service(text):
+    '''
+    функция определяет какую службу вызвать по сообщению о ЧС
+    :param text: сообщение о ЧС
+    :return: название службы "ambulance", "fire" или "police"
+    '''
+    from main import predictor
+    services = ["ambulance", "fire", "police"]
+    if text.strip() != "":
+        rez = predictor.predict([text])[0]
+        return services[rez]
+
+
+class Call(SqlAlchemyBase, SerializerMixin):
+    __tablename__ = 'calls'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    message = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    address = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    point = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    service = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    status = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    call_time = sqlalchemy.Column(sqlalchemy.DateTime,
+                                      default=datetime.datetime.now)
+    finish_time = sqlalchemy.Column(sqlalchemy.DateTime,
+                                     nullable=True)
+
+    def recognize_call(self):
+        '''
+        Заполняет поля address, point, service для нового вызова на основе сообщения и адреса
+        '''
+        # определяем какую службу вызвать
+        self.service = predict_service(self.message)
+        geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+        geocoder_params = {
+            "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+            "geocode": self.address.strip(),
+            "format": "json"}
+
+        response = requests.get(geocoder_api_server, params=geocoder_params)
+
+        if not response:
+            # обработка ошибочной ситуации
+            raise ConnectionError("Не могу проверить адрес")
+
+        # Преобразуем ответ в json-объект
+        json_response = response.json()
+        # Получаем количество топонимов
+        count = int(json_response["response"]["GeoObjectCollection"][
+            "metaDataProperty"]["GeocoderResponseMetaData"]["found"])
+        if count == 0:
+            raise LookupError("Не существует адрес")
+
+        # Получаем топоним
+        toponym = json_response["response"]["GeoObjectCollection"][
+            "featureMember"][0]["GeoObject"]
+        self.point = toponym["Point"]["pos"]
+        self.address = toponym["metaDataProperty"]["GeocoderMetaData"]["text"]
+
+        self.change_status("received")
+
+
+    def change_status(self, new_status):
+        '''
+        Изменяет статус вызова received -> serviced -> finished
+        :param new_status: новый статус
+        '''
+        if new_status == "received":
+            self.call_time = datetime.datetime.now
+        elif new_status == "finished":
+            self.finish_time = datetime.datetime.now
+        self.status = new_status
---
+++ main.py
@@ -0,0 +1,124 @@
+from flask import Flask, render_template, redirect, request, abort, make_response, jsonify
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from data import db_session
+from data.users import User
+from data.calls import Call
+from forms.addcallform import AddCallForm
+from forms.loginform import LoginForm
+from forms.registerform import RegisterForm
+from flask_restful import Api
+import joblib
+
+app = Flask(__name__)
+api = Api(app)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+app.config['JSON_AS_ASCII'] = False
+login_manager = LoginManager()
+login_manager.init_app(app)
+predictor = joblib.load('model/my_log_reg')
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            surname=form.surname.data,
+            position = form.position.data,
+            locality = form.locality.data,
+            email=form.email.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+@app.route('/map')
+@app.route('/')
+def index():
+    #db_sess = db_session.create_session()
+    #jobs = db_sess.query(Jobs).all()
+    #db_sess.commit()
+    #return render_template('map.html', jobs=jobs)
+    return render_template('map.html')
+
+
+@app.route('/add_call',  methods=['GET', 'POST'])
+@login_required
+def add_call():
+    form = AddCallForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        call = Call()
+        call.message = form.message.data
+        call.address  = form.address.data
+        call.recognize_call()
+        db_sess.add(call)
+        db_sess.commit()
+        return redirect('/calls')
+    return render_template('add_call.html', title='Новый вызов',
+                            form=form)
+
+@app.route('/calls')
+def calls():
+    db_sess = db_session.create_session()
+    calls = db_sess.query(Call).all()
+    db_sess.commit()
+    return render_template('calls.html', calls=calls)
+
+
+def main():
+    db_session.global_init("db/emergency.db")
+    # для списка объектов
+    #api.add_resource(UserListResource, '/api/v2/users')
+    #api.add_resource(UserResource, '/api/v2/users/<int:id>')
+    app.run(debug = True)
+
+
+if __name__ == '__main__':
+    main()
+
